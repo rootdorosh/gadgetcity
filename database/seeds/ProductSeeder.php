b6e432adfcc55d09886b84361943496c319642b2
you@example.com <you@example.com>
2020-06-23 18:05:47 +0300

    fixs

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Modules\Product\Models\Product;
+//use DB;
 
 class ProductSeeder extends Seeder
 {
@@ -12,10 +13,18 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('DELETE FROM product');
+        DB::statement('DELETE FROM product_providers_items');
+        DB::statement('UPDATE product_providers SET last_guid = NULL');
+
         $file = base_path() . '/database/data/products.txt';
         $items = file($file);
         foreach ($items as $item) {
             $title = trim($item);
+            if (empty($title)) {
+                continue;
+            }
+
             $is_active = 1;
             $product = Product::updateOrCreate(['title' => $title], compact('title', 'is_active'));
             echo $product->id . "\n";
